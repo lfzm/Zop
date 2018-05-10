7cb80e04f1b6e382dd3960378db8fb00562f6c90
aqa510415008 <510415008@qq.com>
2018-05-10 17:38:17 +0800

    ClaimsPrincipal 扩展类增加ClientId 方法

--- src/Zop.Mvc/Extensions/ClaimsPrincipalExtension.cs
+++ src/Zop.Mvc/Extensions/ClaimsPrincipalExtension.cs
@@ -16,15 +16,28 @@ namespace Microsoft.AspNetCore.Mvc
         /// </summary>
         /// <param name="principal"></param>
         /// <returns></returns>
-        public static int UserId(this ClaimsPrincipal principal)
+        public static string UserId(this ClaimsPrincipal principal)
         {
             var sub = principal.FindFirst(JwtClaimTypes.Subject)?.Value;
             if (string.IsNullOrEmpty(sub))
                 sub = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             if (string.IsNullOrEmpty(sub))
-                return 0;
-            return int.Parse(sub);
+                return null;
+            return sub;
         }
+        /// <summary>
+        /// 客户端Id
+        /// </summary>
+        /// <param name="principal"></param>
+        /// <returns></returns>
+        public static string ClientId(this ClaimsPrincipal principal)
+        {
+            var clientId = principal.FindFirst(JwtClaimTypes.ClientId)?.Value;
+            if (clientId.IsNull())
+                return null;
+            return clientId;
+        }
+        
         /// <summary>
         /// 昵称
         /// </summary>

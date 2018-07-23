15b2ae10ca274ace51d65c6c485f2f5a40a23c2f
aqa510415008 <510415008@qq.com>
2018-07-23 22:32:12 +0800

    优化项目代码结构

--- src/Zop/Domain/Events/EventData.cs
+++ src/Zop/Domain/Events/EventData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Zop.Application.Events
+namespace Zop.Domain.Events
 {
     public abstract class EventData : IEventData
     {
--- src/Zop/Domain/Events/IEventData.cs
+++ src/Zop/Domain/Events/IEventData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Zop.Application.Events
+namespace Zop.Domain.Events
 {
     /// <summary>
     /// 事件数据
